ad07f993ef4ee10c3074b7ef52af7556b6adc9e6
MSebb23 <matteosabbatini23@gmail.com>
2021-05-16 20:49:48 +0200

    First draft
    connected with DB, added few entries and printed all of them.

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+    </head>
+    <body>
+        <h1>HELLO WORLD</h1>
+        @foreach ($movies as $movie)
+            <li>
+                <h2>  {{$movie->title }} </h2> 
+                <p>
+                    Autore:
+                    {{$movie ->author }}
+                </p>
+                <p>
+                    Trama: 
+                    {{$movie ->plot }}
+                </p>
+            </li>
+        @endforeach
+    </body>
+</html>
